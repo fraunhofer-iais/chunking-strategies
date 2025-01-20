742c0576187074eb6a7b76fa9338a02d0503b716
Jannis Spiekermann <jannis.spiekermann@iais.fraunhofer.de>
2025-01-20 11:52:14 +0000

    Integrate hotpot_qa dataset

--- src/dto/dto.py
+++ src/dto/dto.py
@@ -3,10 +3,16 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class Span(BaseModel):
+    start: int
+    end: int
+
+
 class Answer(BaseModel):
     answer: str
     start: Optional[int] = None
     end: Optional[int] = None
+    spans: Optional[list[Span]] = None
 
 
 class EvalSample(BaseModel):
