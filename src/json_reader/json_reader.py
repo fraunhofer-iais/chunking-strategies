0db2d5d925ac2e98df7798f1f9ad8796d0874a43
mrudat-iais <max.rudat@iais.fraunhofer.de>
2025-02-05 11:40:46 +0100

    chore: update comments and requirements

--- src/json_reader/json_reader.py
+++ src/json_reader/json_reader.py
@@ -11,14 +11,11 @@ class JsonReader:
 
     def read_jsons_from_directory(self):
         """Reads all JSON files in a given directory and returns a list of dictionaries.
-
-        Args:
-          directory: The path to the directory containing the JSON files.
-
         Returns:
           A dictionary of JSON filenames mapped to a list of evaluation samples.
         """
-        json_files = [f for f in os.listdir(self._config.json_dir) if f.endswith('.json') and "configs.json" not in f and "scores.json" not in f]
+        json_files = [f for f in os.listdir(self._config.json_dir) if
+                      f.endswith('.json') and "configs.json" not in f and "scores.json" not in f]
         json_data = {}
         for file in json_files:
             file_path = os.path.join(self._config.json_dir, file)
